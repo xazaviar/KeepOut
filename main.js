2e6e28f88f01bcfd66769bdf31cdc94a986cc532
Joseph Ryan <xazaviar@gmail.com>
2020-04-16 14:34:40 -0400

    Data Logging Update
    Added code to log errors and other output to files for later review.
    Also added ip tracking for those trying to break/hack the server (for
    later blacklisting)

--- main.js
+++ main.js
@@ -1,18 +1,57 @@
 const PORT = process.env.PORT || 8080;
 const FPS = 20; // was 60, using reduced to increase performance
+const DEV_MODE = true;
 
 //Dependencies
+const fs = require('fs');
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 const Game = require('./lib/Game');
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, printf } = format;
+ 
+const logFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp} ${level}: ${message}`;
+});
+const consoleFormat = printf(({ level, message}) => {
+  return `${level}: ${message}`;
+});
+
+//Logs
+const logger = createLogger({
+    level: 'info',
+    format: combine(
+        timestamp(),
+        logFormat
+    ),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+        new transports.File({ filename: 'logs/error.log', level: 'error' }),
+        new transports.File({ filename: 'logs/combined.log' })
+    ]
+    // ,
+    // exceptionHandlers: [
+    //     new transports.File({ filename: 'logs/exceptions.log' })
+    // ],
+    // exitOnError: false
+});
+
+if (DEV_MODE) {
+    logger.add(new transports.Console({
+      format: consoleFormat
+    }));
+}else{
+  logger.emitErrs = false;
+}
 
 // Initialization.
 var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
-var game = Game.create();
+var game = Game.create(DEV_MODE, logger);
 
+app.set('trust proxy', true);
 app.set('port', PORT);
 app.use('/public', express.static(__dirname + '/public'));
 
@@ -29,17 +68,18 @@ app.get('/', function(req, res){
 });
 
 app.get('/*', function (req, res, next) {
-  var file = req.params[0];
-  res.sendFile( __dirname + '/public/' + file);
-});
+    var file = req.params[0];
 
+    fs.readFile(__dirname + '/public/' + file, 'utf-8', function (err, data) {
+        var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
 
-//**************************************************************************
-//Files
-//**************************************************************************
-app.get('/*', function (req, res, next) {
-	var file = req.params[0];
-  	res.sendFile( __dirname + '/public/' + file);
+        if(err){
+            logger.error("No such file or directory '"+err.path+"' [REQUEST FROM: "+ip+"]");
+            res.sendFile( __dirname + "/public/404.html" );
+        }else{
+            res.sendFile( __dirname + '/public/' + file);
+        }
+    });
 });
 
 /**
@@ -49,7 +89,7 @@ app.get('/*', function (req, res, next) {
 io.on('connection', (socket) => {
 	socket.on('player-connect', (data) => {
 		//Correct name length
-        data.name = data.name.split(" ").join("_");
+    data.name = data.name.split(" ").join("_");
 		if(data.name.length > 10) data.name = data.name.substring(0,10);
     if(data.name.length < 3) data.name = "???";
 		
@@ -84,5 +124,7 @@ setInterval(() => {
  * Start the server.
  */
 server.listen(PORT, function() {
-  	console.log(`STARTING SERVER ON PORT ${PORT}`);
+    logger.info("***********************************");
+  	logger.info(`STARTING SERVER ON PORT ${PORT}`);
+    logger.info("***********************************");
 });
--- public/js/game/Login.js
+++ public/js/game/Login.js
@@ -1,22 +1,10 @@
 var backColor = {r:0,b:0,g:0}, colorGoal = {r:0,b:0,g:0};
 
 $(document).ready(function() {
-	gapi.load('auth2', function(){
-		gapi.auth2.init();
-	});
 	backColor = {r:parseInt(Math.random()*255),g:parseInt(Math.random()*255),b:parseInt(Math.random()*255)};
     colorGoal = {r:parseInt(Math.random()*255),g:parseInt(Math.random()*255),b:parseInt(Math.random()*255)};
-	setInterval(drawBackground(),200);
+	setInterval(drawBackground,20);
 });
-
-
-function onSignIn(googleUser){
-	var profile = googleUser.getBasicProfile();
-  	console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  	console.log('Name: ' + profile.getName());
-  	console.log('Image URL: ' + profile.getImageUrl());
-  	console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
 
 
 //background coloring
